960b5c19937c83aeb5ab0caa2f87b2e74f32cc93
Chris K. <Chris-SourceCode@outlook.com>
2024-01-21 01:06:51 +0100

    refactor: Change domain for fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [data, setData] = useState("Hello");
 
   const handleClick = async () => {
-    await fetch("http://localhost:5001/")
+    await fetch("https://reland-server.onrender.com/")
       .then((res) => res.json())
       .then((res: { message: string }) => setData(res.message))
       .catch((err) => console.log(err));
